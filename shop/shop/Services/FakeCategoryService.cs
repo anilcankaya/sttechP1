57128e0830ff2c47f0434872f413f11f149ef103
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-15 12:25:15 +0300

    ready for ViewComponent

---
+++ shop/shop/Services/FakeCategoryService.cs
@@ -0,0 +1,28 @@
+﻿using shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.Services
+{
+    public class FakeCategoryService : ICategoryService
+    {
+        private List<Category> categories = null;
+        public FakeCategoryService()
+        {
+            categories = new List<Category>
+            {
+                new Category{ Id=1, Name="Laptop"},
+                new Category{ Id=2, Name="Tablet"},
+                new Category{ Id=3, Name="Telefon"},
+
+
+            };
+        }
+        public IList<Category> GetCategories()
+        {
+            return categories;
+        }
+    }
+}
